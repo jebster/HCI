975100c5283edf6a3fbc60aeddeaed719cb36333
biyan <zhouby23@gmail.com>
2012-10-08 21:53:39 +0800

    Sort Metric View by feelings

--- Frameworks/architecture-examples/HCI Backbone/js/views/activities-metricsView.js
+++ Frameworks/architecture-examples/HCI Backbone/js/views/activities-metricsView.js
@@ -20,13 +20,29 @@ $(function( $ ) {
 		render: function() {
 			$('#activities-metrics ul').html('');	
 			app.Activities.fetch();
+			app.metricViews = [];
 			app.Activities.each(this.addOne, this);
+			app.metricViews.sort(this.compare);
+			app.metricViews.forEach(this.appendOne, this);
 		},
 		
 		addOne: function(activity) {
 			activity.feeling = app.getFeelings(activity);
 			var view = new app.MetricView( { model: activity } );
-//			console.log(view.render().el);
+			app.metricViews.push(view);
+		},
+
+		compare: function(view, view1) {
+			var feeling = view.model.feeling;
+			var feeling1 = view1.model.feeling;
+			if(feeling == feeling1) {
+				return 0;
+			} else if(feeling > feeling1) {
+				return 1;
+			} else return -1;
+		},
+
+		appendOne: function(view) {
 			$('#activities-metrics ul').append(view.render().el);
 		}
 
